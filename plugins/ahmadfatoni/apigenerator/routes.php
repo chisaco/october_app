9fd0560c69794f3050efbb0ec7d0ee06f9933827
Chisako Nishimura <chisaco0403@gmail.com>
2019-10-14 15:56:19 +0900

    add reservations API by API Generator plugin

--- plugins/ahmadfatoni/apigenerator/routes.php
+++ plugins/ahmadfatoni/apigenerator/routes.php
@@ -4,5 +4,5 @@ Route::post('fatoni/generate/api', array('as' => 'fatoni.generate.api', 'uses' =
 Route::post('fatoni/update/api/{id}', array('as' => 'fatoni.update.api', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\ApiGeneratorController@updateApi'));
 Route::get('fatoni/delete/api/{id}', array('as' => 'fatoni.delete.api', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\ApiGeneratorController@deleteApi'));
 
-Route::resource('halo', 'AhmadFatoni\ApiGenerator\Controllers\API\haloController', ['except' => ['destroy', 'create', 'edit']]);
-Route::get('halo/{id}/delete', ['as' => 'halo.delete', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\API\haloController@destroy']);
+Route::resource('api/v1/reservations', 'AhmadFatoni\ApiGenerator\Controllers\API\reservationsController', ['except' => ['destroy', 'create', 'edit']]);
+Route::get('api/v1/reservations/{id}/delete', ['as' => 'api/v1/reservations.delete', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\API\reservationsController@destroy']);
